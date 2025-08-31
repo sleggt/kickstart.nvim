d17b658d18b912ef3ab3cc389e5778504ce1275b
Marco Slegt <mslegt@gmail.com>
2025-08-31 22:46:37 +0200

    add log-highlight.nvim plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -14,6 +14,7 @@ return {
   -- zonder deze plugin was syntax al highlighted, maar
   -- folding werkte niet. Check of folding ook met ingebouwde setting kan werken
   'plasticboy/vim-markdown',
+
   'chrisbra/csv.vim',
   'sleggt/IEC.vim',
   -- 'ionide/Ionide-vim',
@@ -25,5 +26,44 @@ return {
     config = function()
       require('orgmode').setup {}
     end,
+  },
+
+  {
+    'fei6409/log-highlight.nvim',
+    opts = {
+      ---@type string|string[]: File extensions. Default: 'log'
+      extension = 'log',
+
+      ---@type string|string[]: File names or full file paths. Default: {}
+      filename = {
+        'syslog',
+      },
+
+      ---@type string|string[]: File name/path glob patterns. Default: {}
+      pattern = {
+        -- Use `%` to escape special characters and match them literally.
+        '%/var%/log%/.*',
+        'console%-ramoops.*',
+        'log.*%.txt',
+        'logcat.*',
+      },
+
+      ---@type table<string, string|string[]>: Custom keywords to highlight.
+      ---This allows you to define custom keywords to be highlighted based on
+      ---the group.
+      ---
+      ---The following highlight groups are supported:
+      ---    'error', 'warning', 'info', 'debug' and 'pass'.
+      ---
+      ---The value for each group can be a string or a list of strings.
+      ---All groups are empty by default. Keywords are case-sensitive.
+      keyword = {
+        error = 'ERROR_MSG',
+        warning = { 'WARN_X', 'WARN_Y' },
+        info = { 'INFORMATION' },
+        debug = {},
+        pass = {},
+      },
+    },
   },
 }

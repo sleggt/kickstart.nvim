69754282a7a8f1f61e7f05c51004f9c2d30e3d23
Marco Slegt <mslegt@gmail.com>
2025-09-06 19:10:55 +0200

    remove unneeded dependencies from neogit

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -12,9 +12,9 @@ return {
 
       -- Only one of these is needed.
       'nvim-telescope/telescope.nvim', -- optional
-      'ibhagwan/fzf-lua', -- optional
-      'echasnovski/mini.pick', -- optional
-      'folke/snacks.nvim', -- optional
+      -- 'ibhagwan/fzf-lua', -- optional
+      -- 'echasnovski/mini.pick', -- optional
+      -- 'folke/snacks.nvim', -- optional
     },
   },
 

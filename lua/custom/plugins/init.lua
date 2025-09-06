96a142df8d2bba68a17948deb720b0cdc085ecff
Marco Slegt <mslegt@gmail.com>
2025-09-06 18:54:48 +0200

    add Neogit plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -4,6 +4,20 @@
 -- See the kickstart.nvim README for more information
 return {
 
+  {
+    'NeogitOrg/neogit',
+    dependencies = {
+      'nvim-lua/plenary.nvim', -- required
+      'sindrets/diffview.nvim', -- optional - Diff integration
+
+      -- Only one of these is needed.
+      'nvim-telescope/telescope.nvim', -- optional
+      'ibhagwan/fzf-lua', -- optional
+      'echasnovski/mini.pick', -- optional
+      'folke/snacks.nvim', -- optional
+    },
+  },
+
   -- 'justinmk/vim-dirvish',
   -- replaced vim-dirvish with oil
   {
